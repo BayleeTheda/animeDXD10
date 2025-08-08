eaff14cef532a26b07183883047a6a58bdd7a9aa
Christvaldo <christvaldo56@gmail.com>
2025-08-08 15:47:48 +0700

    activate dropdown navbar

--- app/src/main/java/com/kelompok10/animedxd10/MainActivity.java
+++ app/src/main/java/com/kelompok10/animedxd10/MainActivity.java
@@ -1,10 +1,17 @@
 package com.kelompok10.animedxd10;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,6 +19,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.MenuAdapter;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -34,6 +42,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView profile_dropdown = findViewById(R.id.profile_dropdown);
+        RelativeLayout profile_dropdown_expand = findViewById(R.id.profile_dropdown_expand);
+
+        profile_dropdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(profile_dropdown_expand.getVisibility() == GONE){
+                    profile_dropdown_expand.setVisibility(VISIBLE);
+                }else{
+                    profile_dropdown_expand.setVisibility(GONE);
+                }
+            }
+        });
+
+        Intent intent = getIntent();
+
+        TextView profile_name = findViewById(R.id.profile_name);
+        profile_name.setText(intent.getStringExtra("username"));
+
+        CardView logout_btn = findViewById(R.id.logout_btn);
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
